b566755114a186f69201af0fba52c36927e4cd67
spiritxhx <spiritxhx@gmail.com>
2019-06-24 15:30:47 -0400

    add the tail function

---
+++ Week_1/Day_1/tail.js
@@ -0,0 +1,20 @@
+const assertEqual = (actual, expected) => {
+  if (actual === expected) {
+    console.log(`\u{1f600} \u{1f600}  Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`\u{1f602} \u{1f602}  Assertion Failed: ${actual} !== ${expected}`);
+
+  }
+};
+
+const tail = array => {
+  let newArray = [];
+  for (let i = 1; i < array.length; i++) {
+    newArray[i - 1] = array[i];
+  }
+  return newArray;
+};
+
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
